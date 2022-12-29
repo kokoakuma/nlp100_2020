9599aa68da6c87d1f3e3e402642a0ca7580c9cfb
kokoakuma <aikw9101@gmail.com>
2022-12-29 13:10:10 +0000

    add q03

---
+++ c1/q02.py
@@ -0,0 +1,17 @@
+
+import re
+
+def main():
+	text = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+	splittedTexts = re.sub(r"[,.!?:;']", "", text).split(" ")
+
+	sizes = []
+	for text in splittedTexts:
+		textSize = len(text)
+		sizes.append(textSize)
+  
+	for size in sizes:
+		print(str(size))
+
+if __name__ == '__main__':
+    main()
