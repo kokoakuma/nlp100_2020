a25178b403e6ebd7e40c1880666ac08ccee6ffdc
kokoakuma <aikw9101@gmail.com>
2022-12-29 13:32:44 +0000

    add p03.py

--- c1/q02.py
+++ c1/q02.py
@@ -1,17 +1,19 @@
 
 import re
 
-def main():
-	text = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
-	splittedTexts = re.sub(r"[,.!?:;']", "", text).split(" ")
 
-	sizes = []
-	for text in splittedTexts:
-		textSize = len(text)
-		sizes.append(textSize)
-  
-	for size in sizes:
-		print(str(size))
+def main():
+    text = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+    splittedTexts = re.sub(r"[,.!?:;']", "", text).split(" ")
+
+    sizes = []
+    for text in splittedTexts:
+        textSize = len(text)
+        sizes.append(textSize)
+
+    for size in sizes:
+        print(str(size))
+
 
 if __name__ == '__main__':
     main()
